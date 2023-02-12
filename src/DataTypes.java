1d83b5e4da38bf21815b8c88971cee9a8651bdef
Inese Birzniece <inese.birzniece@gmail.com>
2023-02-12 18:37:17 +0200

    initial commit

---
+++ src/DataTypes.java
@@ -0,0 +1,36 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        //Integer Data Type
+        byte num1=100;
+        short num2=1000;
+        int num3=3548564;
+        long num4=44544445856555L;
+
+        //floating point data
+        float num5=125.455f;
+        double num6=-584655.8454588d;
+
+        //boolean data type
+        boolean redLightswitch=false;
+        boolean blackLightSwitch=true;
+
+
+        //character data type
+        char char1='%';
+        char char2='\t';
+        char char3='\n';
+
+        //string
+        String text1="This is text.";
+
+        //print out
+        System.out.println(num1);
+        System.out.println(num6);
+        System.out.println(redLightswitch);
+        System.out.println(blackLightSwitch);
+        System.out.println(char1);
+        System.out.println(char3);
+        System.out.println(text1);
+
+    }
+}
---
+++ src/Lecture2.java
@@ -0,0 +1,7 @@
+public class Lecture2 {
+    public static void main(String[] args) {
+        //Declaring new type integer//
+        int number=10;
+        System.out.println(number);
+    }
+}
