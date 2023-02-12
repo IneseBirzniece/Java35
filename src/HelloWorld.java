0211c0dea8a904e29074d264081f54bfa7fb43fc
Inese Birzniece <inese.birzniece@gmail.com>
2023-02-12 19:29:06 +0200

    second commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World! Un kā ir ar garāku tekstu?");
+        System.out.println("Hello one more time :)");
     }
 }
