3d8a43680f5a9282e4b68cf46546d589b14d8de4
Inese Birzniece <inese.birzniece@gmail.com>
2023-02-12 19:59:00 +0200

    commit addition

--- src/Lecture2.java
+++
@@ -1,7 +0,0 @@
-public class Lecture2 {
-    public static void main(String[] args) {
-        //Declaring new type integer//
-        int number=10;
-        System.out.println(number);
-    }
-}
---
+++ src/MyProject1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class MyProject1 {
+    public static void main(String[] args) {
+        Scanner reader=new Scanner(System.in) ;
+        System.out.println("Enter the number:");
+        int number1 = reader.nextInt();
+        System.out.println("You entered: " + number1);
+
+        System.out.println("Addition of two numbers:");
+        System.out.println("Enter first number:");
+        int number2 = reader.nextInt();
+        System.out.println("Enter second number:");
+        int number3 = reader.nextInt();
+        int sum = number2+number3;
+        System.out.println(number2 + "+" + number3 + "=" + sum);
+
+    }
+}
