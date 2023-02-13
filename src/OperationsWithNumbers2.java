0c007cb09a40576b4a677748a7726d67cd08ad06
Inese Birzniece <inese.birzniece@gmail.com>
2023-02-13 21:26:54 +0200

    feb13-23-3

--- src/OperationsWithNumbers2.java
+++ src/OperationsWithNumbers2.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class OperationsWithNumbers2 {
+import java.util.Scanner;
+
+public class OperationsWithNumbers2 {public static void main(String[] args) {
+    float a, b, c, d, e, g;
+    Scanner reader=new Scanner(System.in) ;
+    System.out.println("Enter 2 decimal numbers:");
+    a = reader.nextFloat();
+    b = reader.nextFloat();
+
+    c = a + b;
+    d = a - b;
+    e = a * b;
+    g = a / b;
+
+    System.out.println("Sum c = " + c);
+    System.out.println("Difference d = " + d);
+    System.out.println("Multiplication e = " + e);
+    System.out.println("Division g = " + g);
+
 }
+}
+
